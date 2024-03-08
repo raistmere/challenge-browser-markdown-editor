2a492a8abb9cea5729267315be72d1c1b6cfb07c
raistmere <raistmere@gmail.com>
2024-03-08 17:07:49 -0600

    feat(component): created header component + finished html/css styling for the component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Header from "./components/Header.tsx";
 
 function App() {
 
   return (
-    <div className="">
-      Hello World
-    </div>
+    <header>
+      <Header />
+    </header>
   )
 }
 
