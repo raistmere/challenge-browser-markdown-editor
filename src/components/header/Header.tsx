723974175fce7006aa94a387ddf798c56b888385
raistmere <raistmere@gmail.com>
2024-03-13 17:48:55 -0500

    feat: added tablet responsive design to the header that reflects what the design document wants

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,7 +3,7 @@ import styles from "./Header.module.css";
 
 const Header = () => {
     return (
-        <div id="HEADER" className={styles.headerWrapper}>
+        <div id="HEADER" className={styles.wrapper}>
             <div className={styles.leftSide}>
                 {/* We want to only show the correct menu button based on if the menu is open or not */}
                 { true ? /* This checks the isMenuOpen state*/
@@ -34,14 +34,11 @@ const Header = () => {
                         <path d="M13.107 3.393c.167.167.31.393.429.678.119.286.178.548.178.786v10.286c0 .238-.083.44-.25.607a.827.827 0 0 1-.607.25h-12a.827.827 0 0 1-.607-.25.827.827 0 0 1-.25-.607V.857C0 .62.083.417.25.25A.827.827 0 0 1 .857 0h8c.238 0 .5.06.786.179.286.119.512.261.678.428l2.786 2.786ZM9.143 1.214v3.357H12.5c-.06-.172-.125-.294-.196-.366L9.509 1.411c-.072-.072-.194-.137-.366-.197Zm3.428 13.643V5.714H8.857a.827.827 0 0 1-.607-.25.827.827 0 0 1-.25-.607V1.143H1.143v13.714H12.57Z" fill="#FFF"/>
                     </svg>
                     <div className={styles.textBox}>
+                        <p className="text-style-body-m">Document Name</p>
                         <h2 className={`text-style-heading-m ${styles.documentName}`}>
                             welcome.md
                         </h2>
                     </div>
-                    {/* <div className={styles.textBox}>
-                        <p>Document Name</p>
-                        <h3>welcome.md</h3>
-                    </div> */}
                 </div>
             </div>
             <div className={styles.rightSide}>
@@ -54,7 +51,7 @@ const Header = () => {
                     <svg className={styles.saveIcon} viewBox="0 0 17 17" xmlns="http://www.w3.org/2000/svg">
                         <path d="M15.91 5.931 10.575.598A.889.889 0 0 0 10.29.41.969.969 0 0 0 9.945.34H2.834A2.667 2.667 0 0 0 .167 3.007v10.666a2.667 2.667 0 0 0 2.667 2.667H13.5a2.667 2.667 0 0 0 2.667-2.667v-7.11a.889.889 0 0 0-.258-.632ZM5.5 2.118h3.556v1.778H5.5V2.118Zm5.334 12.444H5.5v-2.666c0-.491.398-.89.89-.89h3.555c.49 0 .889.399.889.89v2.666Zm3.555-.889c0 .491-.398.89-.889.89h-.889v-2.667a2.667 2.667 0 0 0-2.666-2.667H6.389a2.667 2.667 0 0 0-2.666 2.667v2.666h-.89a.889.889 0 0 1-.888-.889V3.007c0-.491.398-.89.889-.89h.889v2.667c0 .491.398.89.888.89h5.334c.49 0 .889-.399.889-.89V3.371l3.555 3.556v6.746Z" fill="#FFF"/>
                     </svg>
-                    {/* <p>Save Changes</p> */}
+                    <p className="text-style-heading-m">Save Changes</p>
                 </button>
             </div>
         </div>
